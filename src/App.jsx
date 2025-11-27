695a1161e16e56faa09b30bcbc63910bae48340f
muhammad-saad92 <mughalll92@gmail.com>
2025-11-28 02:26:15 +0500

    added the app

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import {useState, useEffect, useCallback, useRef} from 'react';
+
+
+function App() {
+    const [length, setLength] = useState(8);
+    const [charAllowed, setCharAllowed] = useState(false);
+    const [numberAllowed, setNumberAllowed] = useState(false);
+    const [password, setPassword] = useState('');
+
+    const passwordGenerator = useCallback(() => {
+        let pass = ''
+        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        if(numberAllowed) str += '0123456789'
+        if(charAllowed) str += '!@#$%^&*()_=+-}{?'
+
+        for(let i = 0; i <= length; i++){
+            let char = Math.floor(Math.random() * str.length + 1)
+            pass += str.charAt(char)
+        }
+        setPassword(pass)
+    }, [length, numberAllowed, charAllowed, setPassword])
+
+    let passwordRef = useRef(null)
+
+    const copyPasswordToClipboard = useCallback(() => {
+        passwordRef.current?.select();
+        passwordRef.current?.setSelectionRange(0, 999);
+        window.navigator.clipboard.writeText(password)
+    },[password])
+
+    useEffect(() => {
+        passwordGenerator()
+    },[length, numberAllowed, charAllowed, passwordGenerator])
+
+    return(
+        <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+            <h1 className='text-2xl font-bold'>Password Generator</h1>
+            <div className='flex my-8 gap-2'>
+                <input type="text" 
+                value={password}
+                readOnly
+                placeholder='Password'
+                ref={passwordRef}
+                className='bg-white flex-auto rounded-xs outline-none h-9'/>
+                <button 
+                className='flex-auto bg-black text-white rounded-sm h-9'
+                onClick={copyPasswordToClipboard}
+                >Copy</button>   
+            </div>
+            <div>
+                <div>
+                    <input type="range" 
+                    min={6} 
+                    max={100} 
+                    value={length} 
+                    onChange={(e) => {setLength(e.target.value)}}/>
+                    <label>Length: {length}</label>
+                </div>
+                <div>
+                    <input type="checkbox"
+                    defaultChecked = {numberAllowed}
+                    id='numberInput'
+                    onChange={() => {
+                        setNumberAllowed((prev) => !prev)
+                    }}
+                    />
+                    <label htmlFor="numberInput">Numbers</label>
+                </div>
+                <div>
+                    <input type="checkbox"
+                    defaultChecked = {charAllowed}
+                    id='characterInput'
+                    onChange={() => {
+                        setCharAllowed((prev) => !prev)
+                    }}
+                    />
+                    <label htmlFor="characterInput">Characters</label>
+                </div>
+            </div>
+        </div>
+    )
+ 
+}
+
+export default App
+ 
